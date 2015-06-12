d0ac664dbb91f507277abbcb9296a8de6b55e2e6
Benjamin Horn <git@benjaminhorn.se>
2015-06-12 14:12:23 +0300

    Added resource handler.

--- src/Resources.js
+++ src/Resources.js
@@ -1,8 +1,21 @@
+var Cache = require('Cache');
 
-function Resources(room) {
+function Resources(room, population) {
 	this.room = room;
 }
 
-Resources.prototype.
+Resources.prototype.getAvailableResource = function() {
+	// Some kind of unit counter per resource (with Population)
+};
+
+Resources.prototype.getSources = function(room) {
+	return Cache.remember(
+		'resources-sources',
+		function() {
+			return this.room.find(FIND_SOURCES);
+		}.bind(this)
+		
+	);
+};
 
 module.exports = Resources;
