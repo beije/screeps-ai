34dc77778ac4ae17855e45e19e74c8a1e4ba5912
beije <git@benjaminhorn.se>
2015-06-13 12:13:35 +0300

    Implemented soldier.

---
+++ src/CreepSoldier.js
@@ -0,0 +1,55 @@
+var positions = [
+    [0,0],
+    [0,49],
+    [49,49],
+    [49,0]
+];
+
+function CreepSoldier(creep) {
+    this.creep = creep;
+    this.remember('role', 'CreepSoldier');
+};
+
+CreepSoldier.prototype.act = function() {
+    var targets = this.creep.room.find(FIND_HOSTILE_CREEPS);
+    if(this.remember('current-objective') === undefined) {
+        this.remember('current-objective', 0);
+    }
+    if(this.remember('last-position') === undefined) {
+        this.remember('last-position', {x:0, y:0});
+    }
+    if(this.remember('move-attempts') === undefined) {
+        this.remember('move-attempts', 0);
+    }
+
+    if(targets.length) {
+        creep.moveTo(targets[0]);
+        creep.attack(targets[0]);
+    } else {
+        //var exits = Room.find(FIND_EXIT);
+        var lastPosition = this.remember('last-position');
+        var objective = this.remember('current-objective');
+        var moveAttempts = this.remember('move-attempts');
+        if(lastPosition.x == this.creep.pos.x && lastPosition.y == this.creep.pos.y) {
+            movementAttempts++;
+            this.remember('move-attempts', movementAttempts);
+        }
+        if(moveAttempts == 5) {
+            objective++;
+            if(objective >= positions.length) {
+                objective = 0;
+            }
+
+            this.remember('move-attempts', 0);
+            this.remember('last-position', {x:0, y:0});
+            this.remember('last-position', objective);
+
+        } else {
+            this.remember('last-position', {x:this.creep.pos.x, y:this.creep.pos.y});
+        }
+
+        this.creep.moveTo(positions[objective][0], positions[objective][1]);
+    }
+}
+
+module.exports = CreepSoldier;
