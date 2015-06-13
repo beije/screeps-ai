bea973c032fa157a24a6ec3e119f79838d17abff
beije <git@benjaminhorn.se>
2015-06-13 22:44:01 +0300

    Made so the creepBuilder and creepMiner have the same specs.

--- src/CreepFactory.js
+++ src/CreepFactory.js
@@ -68,35 +68,36 @@ CreepFactory.prototype.new = function(creepType, spawn) {
 
 	switch(creepType) {
 		case 'CreepMiner':
+		case 'CreepBuilder':
 			if(level <= 1) {
-				abilities = [WORK, WORK, MOVE];
+				abilities = [WORK, CARRY, MOVE];
 			} else
 			if(level <= 2) {
 				abilities = [WORK, WORK, CARRY, MOVE];
 			} else
 			if(level <= 3) {
-				abilities = [WORK, WORK, CARRY, MOVE];
+				abilities = [WORK, WORK, CARRY, MOVE, MOVE];
 			} else
 			if(level <= 4) {
-				abilities = [WORK, WORK, WORK, CARRY, MOVE];
+				abilities = [WORK, WORK, WORK, CARRY, MOVE, MOVE];
 			} else
 			if(level <= 5) {
-				abilities = [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE];
+				abilities = [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
 			} else
 			if(level <= 6) {
-				abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE];
+				abilities = [WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
 			} else
 			if(level <= 7) {
-				abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, WORK, WORK, MOVE];
+				abilities = [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE];
 			} else
 			if(level <= 8) {
-				abilities = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE];
+				abilities = [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE];
 			} else
 			if(level <= 9) {
-				abilities = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE];
+				abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE];
 			} else
 			if(level >= 10) {
-				abilities = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, MOVE];
+				abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE];
 			}
 		break;
 		case 'CreepCarrier':
@@ -130,39 +131,6 @@ CreepFactory.prototype.new = function(creepType, spawn) {
 			if(level >= 10) {
 				abilities = [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY,  CARRY, MOVE, MOVE, MOVE, MOVE];
 			}
-		break;
-		case 'CreepBuilder':
-			if(level <= 1) {
-				abilities = [WORK, CARRY, MOVE];
-			} else
-			if(level <= 2) {
-				abilities = [WORK, WORK, CARRY, MOVE];
-			} else
-			if(level <= 3) {
-				abilities = [WORK, WORK, CARRY, MOVE, MOVE];
-			} else
-			if(level <= 4) {
-				abilities = [WORK, WORK, WORK, CARRY, MOVE, MOVE];
-			} else
-			if(level <= 5) {
-				abilities = [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
-			} else
-			if(level <= 6) {
-				abilities = [WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
-			} else
-			if(level <= 7) {
-				abilities = [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE];
-			} else
-			if(level <= 8) {
-				abilities = [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE];
-			} else
-			if(level <= 9) {
-				abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE];
-			} else
-			if(level >= 10) {
-				abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE];
-			}
-
 		break;
 		case 'CreepSoldier':
 			if(level <= 1) {
