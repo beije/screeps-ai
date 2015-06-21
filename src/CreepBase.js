04695b8344c5b694541727ec5bb8eea86deff306
beije <git@benjaminhorn.se>
2015-06-21 16:29:09 +0300

    Fixed moved to room method.

--- src/CreepBase.js
+++ src/CreepBase.js
@@ -64,16 +64,24 @@ CreepBase.getAvoidedArea = function() {
 CreepBase.moveToNewRoom = function() {
 	var targetRoom = this.remember('targetRoom');
 	var srcRoom = this.remember('srcRoom');
+	var avoidArea = this.getAvoidedArea();
 
 	if(targetRoom) {
 		if(targetRoom != this.creep.room.name) {
+			this.creep.memory = {};
+			this.remember('srcRoom', srcRoom);
+			this.remember('targetRoom', targetRoom);
+			this.creep.say('Moving');
+
 			var exitDir = this.creep.room.findExitTo(targetRoom);
 			var exit = this.creep.pos.findClosest(exitDir);
-			this.creep.moveTo(exit);
+			this.creep.moveTo(exit, {avoid: avoidArea});
 			return true;
 		} else {
 			this.creep.moveTo(30,30);
-			var targetRoom = this.remember('targetRoom', false);
+			if(this.creep.pos.x < 47 && this.creep.pos.x > 2 && this.creep.pos.y < 47 && this.creep.pos.y > 2) {
+				var targetRoom = this.remember('targetRoom', false);
+			}
 			var srcRoom = this.remember('srcRoom', this.creep.room.name);
 		}
 	} else {
