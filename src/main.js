86762ffae32980bea06f014fc3cc73d4362fe2a0
beije <git@benjaminhorn.se>
2015-06-21 16:32:29 +0300

    Activated scouthandler again.

--- src/main.js
+++ src/main.js
@@ -5,14 +5,13 @@ var Room = require('Room');
 var CreepBase = require('CreepBase');
 var CreepScout = require('CreepScout');
 
-//ScoutHandler.setRoomHandler(RoomHandler);
+ScoutHandler.setRoomHandler(RoomHandler);
 
 // Init rooms
 for(var n in Game.rooms) {
 	var roomHandler = new Room(Game.rooms[n], RoomHandler);
 	RoomHandler.set(Game.rooms[n].name, roomHandler);
 };
-
 // Load rooms
 var rooms = RoomHandler.getRoomHandlers();
 for(var n in rooms) {
@@ -24,12 +23,13 @@ for(var n in rooms) {
 		room.room.name + ' | ' +
 		'goals met:' +
 		room.population.goalsMet() +
+		', Resources: ' + room.resourceManager.getSources().length +
 		', population: ' +
 		room.population.getTotalPopulation() + '/' + room.population.getMaxPopulation() +
 		' (' + room.population.getType('CreepBuilder').total + '/' +
 		room.population.getType('CreepMiner').total + '/' +
 		room.population.getType('CreepCarrier').total + '/' +
-		room.population.getType('CreepSoldier').total + 
+		room.population.getType('CreepSoldier').total +
 		'), ' +
 		'resources at: ' + parseInt( (room.depositManager.energy() / room.depositManager.energyCapacity())*100) +'%, ' +
 		'max resources: ' + room.depositManager.energyCapacity() +'u, ' +
@@ -38,7 +38,7 @@ for(var n in rooms) {
 };
 
 // Load scouts.
-//ScoutHandler.loadScouts();
-//ScoutHandler.spawnNewScouts();
+ScoutHandler.loadScouts();
+ScoutHandler.spawnNewScouts();
 
 HelperFunctions.garbageCollection();
