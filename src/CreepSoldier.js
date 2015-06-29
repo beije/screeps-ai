53f5353910833b4b053f24602fb9487ba3c52b28
beije <git@benjaminhorn.se>
2015-06-29 14:22:33 +0300

    Changed position of soldier.

--- src/CreepSoldier.js
+++ src/CreepSoldier.js
@@ -26,7 +26,7 @@ CreepSoldier.prototype.act = function() {
     if(this.attackHostiles()) { return; }
     if(this.attackSpawns()) { return; }
 
-    this.creep.moveTo(37,40, {avoid: avoidArea});
+    this.creep.moveTo(25,33, {avoid: avoidArea});
 }
 CreepSoldier.prototype.attackHostiles = function() {
     var avoidArea = this.getAvoidedArea();
